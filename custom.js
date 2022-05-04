7c5e7ae452487f73cb5ee49993ae2b26ab40795e
aditi <aditi.tibarewal_ug22@ashoka.edu.in>
2022-05-04 23:08:01 +0530

    commit

--- custom.js
+++ custom.js
@@ -1,11 +1,11 @@
 // JavaScript for enabling the map on load. Change the access token and the web page.
 
-mapboxgl.accessToken = 'Change HERE';
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWRpdGktdGliYXJld2FsIiwiYSI6ImNrb3I3cHRqdDBrZnQyeW5sMnQ0azV6bHIifQ.02TDyY6RrupHOdMXTzeHZA';
 
 window.onload = function() {
   const map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'Change HERE', // style URL
+    style: 'mapbox://styles/aditi-tibarewal/cl2rkmatz000315oxg4ro6e4j', // style URL
     center: [77.0688997, 20.5272803], // starting position [lng, lat]
     zoom: 4 // starting zoom
   });
